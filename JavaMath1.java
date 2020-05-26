842940c89913f81660ded7708306712c29a691ac
RiyaVijayan <63003471+RiyaVijayan@users.noreply.github.com>
2020-05-26 16:46:59 +0530

    Add files via upload

---
+++ JavaMath1.java
@@ -0,0 +1,32 @@
+//Program to illustrate some methods in Java Math class
+
+public class JavaMath1    
+{    
+	public static void main(String[] args)     
+	{    
+		double x = 28;    
+		double y = 4;    
+
+		//maximum of two numbers  
+		System.out.println("Maximum number of x and y is: " +Math.max(x,y));   
+
+		//square root of y   
+		System.out.println("Square root of y is: " + Math.sqrt(y));   
+
+		//x power of 4     
+		System.out.println("Power of x and y is: " + Math.pow(x, y));      
+
+		//logarithm of values       
+		System.out.println("Logarithm of x is: " + Math.log(x));   
+		System.out.println("Logarithm of y is: " + Math.log(y));  
+
+		//logarithm of value with base 10      
+		System.out.println("log10 of x is: " + Math.log10(x));   
+		System.out.println("log10 of y is: " + Math.log10(y));    
+
+		//power of 2    
+		System.out.println("exp of a is: " +Math.exp(x));    
+
+	}    
+}
+    
