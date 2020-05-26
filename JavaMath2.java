be21aa02f80a342d06e41d604b380d55a92974c1
RiyaVijayan <63003471+RiyaVijayan@users.noreply.github.com>
2020-05-26 16:49:28 +0530

    Add files via upload

---
+++ JavaMath2.java
@@ -0,0 +1,37 @@
+public class JavaMathExample2    
+{    
+    public static void main(String[] args)     
+    {    
+        double a = 30;    
+          
+        //radian value    
+        double b = Math.toRadians(a);   
+          
+        //trigonometric sine of a      
+        System.out.println("Sine value of a is: " +Math.sin(a));    
+          
+        //cosine value of a  
+        System.out.println("Cosine value of a is: " +Math.cos(a));  
+          
+        //tangent value of a  
+        System.out.println("Tangent value of a is: " +Math.tan(a));  
+          
+        //arc sine of a      
+        System.out.println("Sine value of a is: " +Math.asin(a));    
+          
+        //arc cosine value of a  
+        System.out.println("Cosine value of a is: " +Math.acos(a));  
+          
+        //arc tangent value of a  
+        System.out.println("Tangent value of a is: " +Math.atan(a));  
+  
+        //hyperbolic sine of a      
+        System.out.println("Sine value of a is: " +Math.sinh(a));    
+          
+        //hyperbolic cosine value of a  
+        System.out.println("Cosine value of a is: " +Math.cosh(a));  
+          
+        //hyperbolic tangent value of a  
+        System.out.println("Tangent value of a is: " +Math.tanh(a));  
+    }    
+}  
